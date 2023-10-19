fd92ddb3c8cc41d95aebf4fd4e5995b304356bf2
Lizeth Lopez <lizlopez.10@hotmail.com>
2023-10-19 00:22:36 -0500

    config yml

--- db/db.js
+++ db/db.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', 'admin', {
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres'
-});
-
-// const sequelize = new Sequelize(process.env.NAME_DB, process.env.USER_DB, process.env.PASSWORD_DB, {
-//   host: process.env.HOST_DB,
-//   port: process.env.PORT_DB,
+// const sequelize = new Sequelize('postgres', 'postgres', 'admin', {
+//   host: 'localhost',
+//   port: 5432,
 //   dialect: 'postgres'
 // });
+
+const sequelize = new Sequelize(process.env.NAME_DB, process.env.USER_DB, process.env.PASSWORD_DB, {
+  host: process.env.HOST_DB,
+  port: process.env.PORT_DB,
+  dialect: 'postgres'
+});
  
 module.exports = sequelize
