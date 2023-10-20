94d6a8cdaec824a4452c2ff2ab7140a825c7c2a4
ErikBernal94 <ed.bernalc1@uniandes.edu.co>
2023-10-20 01:01:42 -0500

    route changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/metadata', metadataRouter);
+app.use('/empresa/metadata', metadataRouter);
 // app.use('/evaluacion/pregunta', preguntasRouter);
 app.use('/empresa/healthcheck', healthcheckRouter);
 
