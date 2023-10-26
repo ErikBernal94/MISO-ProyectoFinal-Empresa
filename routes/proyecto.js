d1d1d06b87eb32f1c85c2611d2da6af1c2c55a59
Lizeth Lopez <lizlopez.10@hotmail.com>
2023-10-25 21:24:18 -0500

    add project logic

---
+++ routes/proyecto.js
@@ -0,0 +1,17 @@
+var logicaMetadata = require ('../logic/proyecto.logic');
+
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', async function(req, res) {
+    var result = await logicaMetadata.obtener();
+    if(!result){
+        res.status(400).send();    
+    }
+    res.status(200).send(result);
+});
+
+module.exports = router;
+
+
